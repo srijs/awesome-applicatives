9bd270f0fd823b8ec053233a1f6afb6d4df69c47
Sam Rijs <srijs@airpost.net>
2016-09-04 23:16:42 +1000

    add free applicative

--- src/Average.hs
+++ src/Average.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving #-}
 
 
 module Average where
 
 
 import Prelude hiding (sum)
+import Control.Applicative
 import Data.Ratio
 
 
@@ -41,3 +42,66 @@ count =
 average :: Integral a => Aggregation a (Ratio a)
 average =
   (%) <$> sum <*> count
+
+
+------
+
+
+data Ap f a where
+  Pure :: a -> Ap f a
+  Ap :: f a -> Ap f (a -> b) -> Ap f b
+
+
+instance Functor (Ap f) where
+  fmap f x =
+    case x of
+      Pure a ->
+        Pure (f a)
+
+      Ap y z ->
+        Ap y ((\g -> f . g) <$> z)
+
+
+instance Applicative (Ap f) where
+  pure =
+    Pure
+
+  l <*> r =
+    case l of
+      Pure f ->
+        fmap f r
+
+      Ap x y ->
+        Ap x (flip <$> y <*> r)
+
+
+liftAp :: f a -> Ap f a
+liftAp x =
+  Ap x (Pure id)
+
+
+retractAp :: Applicative f => Ap f a -> f a
+retractAp x =
+  case x of
+    Pure a ->
+      pure a
+
+    Ap y z ->
+      y <**> retractAp z
+
+
+-----
+
+
+newtype ParallelAggregation a b = PAgg (Ap (Aggregation a) b)
+  deriving (Functor, Applicative)
+
+
+toParallel :: Aggregation a b -> ParallelAggregation a b
+toParallel agg =
+  PAgg (liftAp agg)
+
+
+fromParallel :: ParallelAggregation a b -> Aggregation a b
+fromParallel (PAgg ap) =
+  retractAp ap
